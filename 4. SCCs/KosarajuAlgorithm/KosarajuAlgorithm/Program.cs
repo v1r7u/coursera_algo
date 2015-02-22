43997a9ddc530b3c8dea53f7bf21854e879801fb
ik <igor.kliushnikov@hotmail.com>
2015-02-22 15:11:55 +0200

    dirty hack: new thread with huge stack size

--- 4. SCCs/KosarajuAlgorithm/KosarajuAlgorithm/Program.cs
+++ 4. SCCs/KosarajuAlgorithm/KosarajuAlgorithm/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using KosarajuAlgorithm.GraphClasses;
 
 namespace KosarajuAlgorithm
@@ -11,26 +12,50 @@ namespace KosarajuAlgorithm
         //private const int vertexCount = 9;
         private const int vertexCount = 875714;
 
-        static void Main(string[] args)
+        static void Main()
         {
-            var graph = GraphWithLeader();
 
-            graph.DFSLoop();
+            Thread thread = new Thread(GetSCC, 10000000);
+            thread.Start();
+            thread.Join();
 
             Console.ReadKey();
         }
 
-        private static Graph GraphWithLeader()
+        private static void GetSCC()
         {
+            Console.WriteLine("Started:{0}", DateTime.Now);
             const string path = @"E:\projects\coursera_algo\4. SCCs\SCC.txt";
             //const string path = @"E:\projects\coursera_algo\4. SCCs\3_3_3_0_0.txt";
             var _initialGraph = ParseInitialFile(path);
 
+            Console.WriteLine("File Parsed at:{0}", DateTime.Now);
             _initialGraph.DFSLoop();
-            
+
+            Console.WriteLine("Finishing times calculated at:{0}", DateTime.Now);
             var _reversedGraph = Reverse(_initialGraph);
 
-            return _reversedGraph;
+            Console.WriteLine("Graph reversed at:{0}", DateTime.Now);
+            _reversedGraph.DFSLoop();
+
+            Console.WriteLine("Leaders calculated at:{0}", DateTime.Now);
+
+            Dictionary<int, int> sccs = new Dictionary<int, int>();
+            foreach (var vertex in _reversedGraph.Vertices)
+            {
+                if (sccs.Keys.Contains(vertex.Leader.Name))
+                {
+                    sccs[vertex.Leader.Name]++;
+                }
+                else
+                {
+                    sccs.Add(vertex.Leader.Name, 1);
+                }
+            }
+            var leaders = sccs.Values.OrderByDescending(i => i).Take(5).ToArray();
+            var format = string.Join(",", leaders);
+            Console.WriteLine("SCCs calculated at:{0}", DateTime.Now);
+            Console.WriteLine(format);
         }
 
         private static Graph ParseInitialFile(string path)
